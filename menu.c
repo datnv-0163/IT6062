2b2273448514d18c65d3c10f79fc5164ce667347
bachtung144 <bachtung144007@gmail.com>
2021-01-08 17:23:50 +0700

    add color

--- menu.c
+++ menu.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include "color.h"
+
 void menu(){
-	printf("\nUSER MANAGERMENT PROGRAM\n");
-	printf("-----------------------------------\n");
-	printf("1. Register\n");
-	printf("2. Sign in\n");
-	//printf("3. Sign out\n");
-	printf("3. Exit\n");
-	printf("Your choice (1-3):  ");
+	printf(MAG"\nUSER MANAGERMENT PROGRAM\n");
+	printf(MAG"-----------------------------\n");
+	printf(GRN"1. Register\n");
+	printf(YEL"2. Sign in\n");
+	printf(RED"3. Exit\n");
+	printf(MAG"Your choice (1-3):  ");
 }
 
 void chatFunction(){
-	printf("\nCHATROOM FUNCTION\n");
-	printf("-----------------------------------\n");
-	printf("0. List member Online\n");
-	printf("1. Create room\n");
-	printf("2. Get in room\n");
-	printf("3. List of rooms\n");
-	printf("4. Chat 1vs1\n");
-	printf("5. Sign Out\n");
-	printf("Your choice (0-5):\n");
-	//printf("a");
+	printf(MAG"\nCHATROOM FUNCTION\n");
+	printf(MAG"---------------------------\n");
+	printf(YEL"0. List member Online\n");
+	printf(BLU"1. Create room\n");
+	printf(GRN"2. Get in room\n");
+	printf(YEL"3. List of rooms\n");
+	printf(CYN"4. Single chat\n");
+	printf(RED"5. Sign Out\n");
+	printf(MAG"Your choice (0-5):\n");
 }
